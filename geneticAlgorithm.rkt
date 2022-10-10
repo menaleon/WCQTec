536411b87fba5a26846b565e936f682fa729858b
Justin Fernández <jusfb18@gmail.com>
2022-10-10 03:35:58 -0600

    Conversion from binary to decimal and viceversa, binary sum of 4 bits and overflow

--- geneticAlgorithm.rkt
+++ geneticAlgorithm.rkt
@@ -117,8 +117,76 @@
   (cadr (cddddr (cdddr player)))
   )
 
-(selection-aux '(4 4 2) 'CR)
+
+
+(define (convertBinary number)
+  (cond ((zero? number) '(0))
+        ((equal? number 1) '(1))
+        (else
+         (append (convertBinary (truncate (/ number 2))) (list(remainder number 2)))
+)))
+
+(define (convertDecimal binaryNum)
+  (convertDecimal-aux binaryNum 3)
+  )
+
+(define (convertDecimal-aux binaryNum power)
+  (cond ((null? binaryNum) 0)
+        ((equal? (car binaryNum) 0) (convertDecimal-aux (cdr binaryNum) (- power 1)))
+        (else
+         (+ (powerTwo power) (convertDecimal-aux (cdr binaryNum) (- power 1)))
+  )))
+
+(define (powerTwo x)
+    (cond ((zero? x)  1)
+      ( else
+          (* 2 (powerTwo (- x 1))))))
+
+(define (dischardOverflow binaryNumber)
+  (cond ((> (convertDecimal binaryNumber) 10) '(1 0 1 0))
+        (else
+         binaryNumber
+  )))
+
+
+(define (mutation)
+  (display "Here goes mutation")
+  )
+
+(define (mutation-aux gen randomBit)
+  (cond ((equal? randomBit 0)
+         (binarySum gen randomBit))
+  ))
+
+(define (binarySum number bitPos)
+  (cond ((zero? bitPos)
+         (cond ((equal? (+ (cadddr number) 1) 2) (binarySum (append (append (append (list(car number)) (list(cadr number))) (list(caddr number))) '(0)) (+ bitPos 1)))
+               (else
+                (dischardOverflow(append (append (append (list(car number)) (list(cadr number))) (list(caddr number))) '(1)))
+         )))
+        ((equal? bitPos 1)
+         (cond ((equal? (+ (caddr number) 1) 2) (binarySum (append (append (append (list(car number)) (list(cadr number))) '(0)) (list(cadddr number))) (+ bitPos 1)))
+               (else
+                dischardOverflow((append (append (append (list(car number)) (list(cadr number))) '(1)) (list(cadddr number))))))
+         )
+        ((equal? bitPos 2)
+         (cond ((equal? (+ (cadr number) 1) 2) (binarySum (append (append (append (list(car number)) '(0)) (list(caddr number))) (list(cadddr number))) (+ bitPos 1)))
+               (else
+                (dischardOverflow (append (append (list(car number)) '(1)) (cddr number)))
+          )))
+        ((equal? (+ (car number) 1) 2) (append '(0) (cdr number)))
+              (else
+         (dischardOverflow (append '(1) (cdr number))))
+  ))
+
+;;(selection-aux '(4 4 2) 'CR)
 ;;(selection-aux '(5 4 1) 'SPA)
 ;;(selection-aux '(3 4 3) 'ENG)
-(getPlayerPosX '(CR 5 forward 3 9 6 50 20 3))
-(getPlayerGen '(CR 5 forward 3 9 6 50 20 3))
+;;(getPlayerPosX '(CR 5 forward 3 9 6 50 20 3))
+;;(getPlayerGen '(CR 5 forward 3 9 6 50 20 3))
+;;(convertBinary '3)
+;;(convertDecimal '(0 1 1 1))
+;;(dischardOverflow '(1 0 1 1))
+;;(mutation-aux '(0 1 1 1) 3)
+
+(binarySum '(1 0 1 0) '0)
