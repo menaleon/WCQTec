c8aca0194eb890fc4b8cb85eb5327f8f5edc558f
Justin Fernández <jusfb18@gmail.com>
2022-10-10 02:11:57 -0600

    Functions to get gens and each kind of player

--- geneticAlgorithm.rkt
+++ geneticAlgorithm.rkt
@@ -35,31 +35,90 @@
   (list (random 10))
   )
 
-;; genes por orden (equipo numeroJugador tipoJugador velocidad fuerza habilidad posX posY numGen)
+;; genes por orden (equipo numero tipoJugador velocidad fuerza habilidad posX posY numGen)
 
 ;; createGoalKeeper: creates the goalkeeper
 (define (createGoalKeeper team)
   (append (append (append (append (append (append (append (append (list team) '(1)) (list 'keeper)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) '(1))
 )
+
+
 ;; createDefenders: creates the defenders
 (define (createDefenders team num)
  (cond ((equal? num 0) '())
        (else
-        (cons (append (append (append (append (append (append (append (append (list team) '(1)) (list 'defender)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) (list (+ num 1))) (createDefenders team (- num 1))))
+        (cons (append (append (append (append (append (append (append (append (list team) (list (+ num 1))) (list 'defender)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) '(1)) (createDefenders team (- num 1))))
   ))
 ;; createMidFielders: creates the midfielders
 (define (createMidFielders team num)
  (cond ((equal? num 0) '())
        (else
-        (cons (append (append (append (append (append (append (append (append (list team) '(1)) (list 'mid)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) (list (+ num 1))) (createMidFielders team (- num 1))))
+        (cons (append (append (append (append (append (append (append (append (list team) (list (+ num 1))) (list 'mid)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) '(1)) (createMidFielders team (- num 1))))
   ))
 ;; createForwards: creates the forwards
 (define (createForwards team num)
  (cond ((equal? num 0) '())
        (else
-        (cons (append (append (append (append (append (append (append (append (list team) '(1)) (list 'forward)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) (list (+ num 1))) (createForwards team (- num 1))))
-  ))
+        (cons (append (append (append (append (append (append (append (append (list team) (list (+ num 1))) (list 'forward)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) '(1)) (createForwards team (- num 1)))))
+  )
+
+
+
+(define (getKeeper teamPlayers)
+  (car teamPlayers)
+)
+
+(define (getDefenders teamPlayers)
+  (cadr teamPlayers)
+)
+
+(define (getMids teamPlayers)
+  (caddr teamPlayers)
+)
+
+(define (getForwards teamPlayers)
+  (cadddr teamPlayers)
+)
+
+;; genes por orden (equipo numeroJugador tipoJugador velocidad fuerza habilidad posX posY numGen)
+(define (getPlayerTeam player)
+  (car player)
+  )
+
+(define (getPlayerNum player)
+  (cadr player)
+  )
+
+(define (getPlayerType player)
+  (caddr player)
+  )
+
+(define (getPlayerVel player)
+  (cadddr player)
+  )
+
+(define (getPlayerForce player)
+  (cadr (cdddr player))
+  )
+
+(define (getPlayerAbility player)
+  (caddr (cdddr player))
+  )
+
+(define (getPlayerPosX player)
+  (cadddr (cdddr player))
+  )
+
+(define (getPlayerPosY player)
+  (car (cddddr (cdddr player)))
+  )
+
+(define (getPlayerGen player)
+  (cadr (cddddr (cdddr player)))
+  )
 
 (selection-aux '(4 4 2) 'CR)
-(selection-aux '(5 4 1) 'SPA)
-(selection-aux '(3 4 3) 'ENG)
+;;(selection-aux '(5 4 1) 'SPA)
+;;(selection-aux '(3 4 3) 'ENG)
+(getPlayerPosX '(CR 5 forward 3 9 6 50 20 3))
+(getPlayerGen '(CR 5 forward 3 9 6 50 20 3))
