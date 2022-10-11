38f1c022b27138329b6493611806df5c4b7fdd3b
Jimena León <jimenaleon03@gmail.com>
2022-10-10 20:21:40 -0600

    Arrengements and comments
    To improve legibility

--- geneticAlgorithm.rkt
+++ geneticAlgorithm.rkt
@@ -1,23 +1,6 @@
 #lang racket/gui
-;; numDefenders?: gives the number of defenders
-(define (numDefenders? estrategy)
-  (car estrategy)
-  )
 
-;; numMidFielders?: gives the number of midfielders
-(define (numMidFielders? estrategy)
-  (cadr estrategy)
-  )
-
-;; numForwards?: gives the number of forwards
-(define (numForwards? estrategy)
-  (caddr estrategy)
-  )
-
-;; selection-aux:
-(define (selection-aux estrategy team)
-  (createFirstGen (numDefenders? estrategy) (numMidFielders? estrategy) (numForwards? estrategy) team)
-  )
+;; MAIN GENETIC FUNCTIONS----------------------------------------------------------------------------------
 
 ;; createFirstGen: creates the first generation for each team according to a given estrategy
 ;; numDefenders: number of defenders of the estrategy
@@ -26,14 +9,40 @@
 ;; team: number or name of the team
 (define (createFirstGen numDefenders numMidFielders numForwards team)
   (append (list team) (append (append (append (list(createGoalKeeper team)) (list (createDefenders team numDefenders))) (list (createMidFielders team numMidFielders)))
-                              (list (createForwards team numForwards)) ))
+                              (list (createForwards team numForwards)) ) )
 )
 
-
-;; asignVelocityGen: random
-(define (randomValue)
-  (list (random 10))
+(define (aptitude player)
   )
+
+(define (aptitude-goalKeeper player)
+  )
+
+(define (aptitude-defender player)
+  )
+
+(define (aptitude-forward player)
+  )
+
+(define (selection )
+  )
+;; implement iterative function for the algorithm
+
+(define (selection-aux estrategy team)
+  (createFirstGen (numDefenders? estrategy) (numMidFielders? estrategy) (numForwards? estrategy) team)
+  )
+
+(define (mutation)
+  (display "Here goes mutation")
+  )
+
+(define (mutation-aux gen randomBit)
+  (cond ((equal? randomBit 0)
+         (binarySum gen randomBit))
+  ))
+
+
+;; CREATE LIST OF A SPECIFIC TYPE OF PLAYERS---------------------------------------------------------------------
 
 ;; genes por orden (equipo numero tipoJugador velocidad fuerza habilidad posX posY numGen)
 
@@ -41,7 +50,6 @@
 (define (createGoalKeeper team)
   (append (append (append (append (append (append (append (append (list team) '(1)) (list 'keeper)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) '(1))
 )
-
 
 ;; createDefenders: creates the defenders
 (define (createDefenders team num)
@@ -62,7 +70,29 @@
         (cons (append (append (append (append (append (append (append (append (list team) (list (+ num 1))) (list 'forward)) (randomValue)) (randomValue)) (randomValue)) '(50)) '(20)) '(1)) (createForwards team (- num 1)))))
   )
 
+;; asignVelocityGen: random
+(define (randomValue)
+  (list (random 10))
+  )
 
+;; OBTAIN HOW MANY PLAYERS OF A SPECIFIC TYPE-----------------------------------------------------
+
+;; numDefenders?: gives the number of defenders
+(define (numDefenders? estrategy)
+  (car estrategy)
+  )
+
+;; numMidFielders?: gives the number of midfielders
+(define (numMidFielders? estrategy)
+  (cadr estrategy)
+  )
+
+;; numForwards?: gives the number of forwards
+(define (numForwards? estrategy)
+  (caddr estrategy)
+  )
+
+;; OBTAIN PLAYERS OF A SPECIFIC TYPE, FROM THE TREE------------------------------------------------
 
 (define (getKeeper teamPlayers)
   (car teamPlayers)
@@ -79,6 +109,8 @@
 (define (getForwards teamPlayers)
   (cadddr teamPlayers)
 )
+
+;; OBTAIN GENES OF A SINGLE PLAYER------------------------------------------------------------------
 
 ;; genes por orden (equipo numeroJugador tipoJugador velocidad fuerza habilidad posX posY numGen)
 (define (getPlayerTeam player)
@@ -118,6 +150,7 @@
   )
 
 
+;; DEALING WITH BINARY ARITHMETIC (for selection, reproduction and mutation)------------------------------------------
 
 (define (convertBinary number)
   (cond ((zero? number) '(0))
@@ -148,16 +181,6 @@
          binaryNumber
   )))
 
-
-(define (mutation)
-  (display "Here goes mutation")
-  )
-
-(define (mutation-aux gen randomBit)
-  (cond ((equal? randomBit 0)
-         (binarySum gen randomBit))
-  ))
-
 (define (binarySum number bitPos)
   (cond ((zero? bitPos)
          (cond ((equal? (+ (cadddr number) 1) 2) (binarySum (append (append (append (list(car number)) (list(cadr number))) (list(caddr number))) '(0)) (+ bitPos 1)))
@@ -179,9 +202,9 @@
          (dischardOverflow (append '(1) (cdr number))))
   ))
 
-;;(selection-aux '(4 4 2) 'CR)
-;;(selection-aux '(5 4 1) 'SPA)
-;;(selection-aux '(3 4 3) 'ENG)
+(selection-aux '(4 4 2) 'CR)
+(selection-aux '(5 4 1) 'SPA)
+(selection-aux '(3 4 3) 'ENG)
 ;;(getPlayerPosX '(CR 5 forward 3 9 6 50 20 3))
 ;;(getPlayerGen '(CR 5 forward 3 9 6 50 20 3))
 ;;(convertBinary '3)
@@ -189,4 +212,5 @@
 ;;(dischardOverflow '(1 0 1 1))
 ;;(mutation-aux '(0 1 1 1) 3)
 
-(binarySum '(1 0 1 0) '0)
+(binarySum '(1 0 1 0) '1)
+
