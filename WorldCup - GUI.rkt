b7368c7d4c586fea58a977bdf78d708fc0c75a49
eduardo-bm10 <ebolivarminguet@gmail.com>
2022-10-11 20:35:43 -0600

    Update WorldCup - GUI.rkt

--- WorldCup - GUI.rkt
+++ WorldCup - GUI.rkt
@@ -7,10 +7,22 @@
        [width 1000]
        [height 700]))
 
-;; Campo de juego
-(define field
-  (new canvas%
+(define mainpane
+  (new pane%
        [parent frame]
+       [min-width 1000]
+       [min-height 565]))
+
+(define menu
+  (new pane%
+       [parent frame]
+       [min-width 1000]
+       [min-height 135]))
+
+;; Campo de juego
+(define field-canvas
+  (new canvas%
+       [parent mainpane]
        [paint-callback (lambda (canvas dc)
                          (send dc set-brush "green" 'solid)
                          (send dc draw-rectangle 0 0 1000 565) ;; Se dibuja el campo de juego.
@@ -26,7 +38,12 @@
                          (send dc draw-rectangle 880 200 100 200) ;; Zona de portero.
                          (send dc set-pen "brown" 10 'solid)
                          (send dc draw-line 10 200 10 400) ;; Marco de gol.
-                         (send dc draw-line 970 200 970 400) ;; Marco de gol.
+                         (send dc draw-line 970 200 970 400))]))
+
+(define menu-canvas
+  (new canvas%
+       [parent menu]
+       [paint-callback (lambda (canvas dc)
                          (send dc set-brush "gray" 'solid)
                          (send dc set-pen "green" 1 'transparent)
                          (send dc draw-rectangle 0 565 1000 700) ;; Menú inferior
@@ -38,44 +55,48 @@
                          (send dc set-text-foreground "white")
                          (send dc draw-text "0" 400 600)
                          (send dc draw-text "0" 590 600))]))
+                       
+(define (draw-object plx ply ballx bally)
+  (send (send field-canvas get-dc) set-pen "brown" 10 'transparent)
+  (send (send field-canvas get-dc) set-brush "pink" 'solid)
+  (send (send field-canvas get-dc) draw-rectangle plx ply 20 15)
+  (send (send field-canvas get-dc) set-brush "blue" 'solid)
+  (send (send field-canvas get-dc) draw-rectangle plx (+ ply 15) 20 30)
+  (send (send field-canvas get-dc) set-brush "black" 'solid)
+  (send (send field-canvas get-dc) draw-rectangle plx (+ ply 45) 20 10)
+  (send (send field-canvas get-dc) set-brush "white" 'solid)
+  (send (send field-canvas get-dc) set-pen "black" 2 'solid)
+  (send (send field-canvas get-dc) draw-ellipse ballx bally 25 25)
+  (sleep 0.007)
+  (send (send field-canvas get-dc) erase)
+  (send field-canvas on-paint))
+         
+(define (render-menu seconds)
+  (send (send menu-canvas get-dc) draw-text (~a seconds) 495 600)
+  (sleep 1)
+  (send (send menu-canvas get-dc) erase)
+  (send menu-canvas on-paint))
 
-(define (draw-player x y)
-  (send (send field get-dc) set-brush "pink" 'solid)
-  (send (send field get-dc) draw-rectangle x y 20 15)
-  (send (send field get-dc) set-brush "blue" 'solid)
-  (send (send field get-dc) draw-rectangle x (+ y 15) 20 30)
-  (send (send field get-dc) set-brush "black" 'solid)
-  (send (send field get-dc) draw-rectangle x (+ y 45) 20 10)
-  (sleep 0.005)
-  (send (send field get-dc) erase)
-  (send field on-paint))
-
-(define (draw-ball x y)
-  (send (send field get-dc) set-brush "white" 'solid)
-  (send (send field get-dc) set-pen "black" 2 'solid)
-  (send (send field get-dc) draw-ellipse 20 20 150 150))
+(define (update-menu seconds)
+  (cond ((zero? seconds)
+         (render-menu seconds))
+        (else
+         (render-menu seconds)
+         (update-menu (- seconds 1)))))
+          
+(define (threaded-menu)
+  (update-menu 90))
 
 (define (anim)
-  (cond ((>= posx 980)
-         (draw-player posx posy))
+    (anim_aux 30 30 50 50))
+
+(define (anim_aux plx ply bx by)
+  (cond ((>= plx 1000)
+         (draw-object plx ply bx by))
         (else
-         (draw-player posx posy)
-         (anim (+ posx 1) posy))))
-
-(define (render seconds)
-  (send (send field get-dc) draw-text (~a seconds) 495 600)
-  (sleep 1))
-
-(define (update seconds)
-  (cond ((zero? seconds)
-         (render seconds))
-        (else
-         (render seconds)
-         (update (- seconds 1)))))
-
-(define (func-timer)
-  (update 90))
+         (draw-object plx ply bx by)
+         (anim_aux (+ plx 1) (+ ply 1) (+ bx 1) (+ by 1)))))
 
 (send frame show #t)
-(thread func-timer)
+(thread threaded-menu)
 (thread anim)
