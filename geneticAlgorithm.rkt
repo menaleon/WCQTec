62fbc1d914460e54731334d36203a69be8dfd341
Justin Fernández <jusfb18@gmail.com>
2022-10-16 16:14:29 -0600

    fixes to mutation pos

--- geneticAlgorithm.rkt
+++ geneticAlgorithm.rkt
@@ -1,5 +1,6 @@
 #lang racket/gui
 
+;; createFirstGen: creates the first generation for each team according to a given estrategy
 (define (createFirstGen numDefenders numMidFielders numForwards team)
   (append (list team)
           (list (createGoalKeeper team))
@@ -40,9 +41,9 @@
 
 (define (selection-rec players-type fitPlayers)
   (cond ((null? players-type) fitPlayers)
-        ((aptitude? (car players-type)) (selection-rec (cdr players-type) (cons (car players-type) fitPlayers)))
         (else (selection-rec (cdr players-type) fitPlayers))))
 
+; reproduction makes 2 children (not less, not more)
 (define (reproduce selectedPlayers children)
   (cond ((or (null? (car selectedPlayers)) (null? (cdr selectedPlayers))) children)
         (else
@@ -64,6 +65,7 @@
       (convertDecimal (append (list (car binaryGenPlayer1) (cadr binaryGenPlayer1))
                               (list (caddr binaryGenPlayer2) (cadddr binaryGenPlayer2)))))
 
+; update team-tree by checking playerNum. Looks for childrenPlayer num in the tree and updates that in that position. recursive
 (define (updateTree team-tree children)
   (cond ((null? children) team-tree)
         ((equal? (getPlayerType (car children)) 'keeper)
@@ -82,21 +84,25 @@
 
 ;; Functions to make mutation
 (define (mutation player)
+(define (mutationPlayer player)
   (append (list(getPlayerTeam player))
           (list(getPlayerNum player))
           (list(getPlayerType player))
-          (mutateSpecificGen (getPlayerVel player))
-          (mutateSpecificGen (getPlayerForce player))
-          (mutateSpecificGen (getPlayerAbility player))
-          (list(getPlayerPosX player))
-          (list(getPlayerPosY player))
+          (mutateSpecificGen (getPlayerVel player) 0)
+          (mutateSpecificGen (getPlayerForce player) 0)
+          (mutateSpecificGen player 2)
           (list(getPlayerGen player))))
 
-(define (mutateSpecificGen numberOfGen)
-  (list (convertDecimal (mutation-aux (convertBinary numberOfGen) (random 4))))  )
 
-(define (mutation-aux gen randomBit)
   (binarySum gen randomBit))
+                (cond ((equal? (getPlayerType player) 'keeper) (mutation-aux (convertBinary_aux (getPlayerPosY player) 10) (random 9) '(200) '(400)))
+                      (else
+                       (mutation-aux (convertBinary_aux (getPlayerPosY player) 10) (random 9) '(0) '(565))
+                      (else
+                      ((equal? (getPlayerType player) 'defender) (mutation-aux (convertBinary_aux (getPlayerPosX player) 10) (random 9) '(100) '(300)))
+               (else
+                (cond ((equal? (getPlayerType player) 'keeper) (mutation-aux (convertBinary_aux (getPlayerPosX player) 10) (random 9) '(880) '(980)))
+               )))))                          
 
 (define (geneticAlgorithm team-tree)
   (updateTree team-tree (reproduce (selection team-tree) '())))
@@ -140,6 +146,8 @@
 
 (define (randomFloat)
   (/ (random 4294967087) 4294967086.0))
+     ;         ((equal? typePlayer 'defender) ())
+     ;(cond
 
 (define (randomPos minPos maxPos)
   (list (exact-round (- maxPos (* minPos (randomFloat))))))
@@ -236,16 +244,167 @@
          )))
         ((equal? bitPos 1)
          (cond ((equal? (+ (caddr number) 1) 2) (binarySum (append (append (append (list(car number)) (list(cadr number))) '(0)) (list(cadddr number))) (+ bitPos 1)))
+                                   (+ bitPos 1) minValue maxValue))
                (else
-                (dischardOverflow(append (append (append (list(car number)) (list(cadr number))) '(1)) (list(cadddr number))))))
-         )
+                (dischardOverflow (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                   (indexLista number '5) (indexLista number '6) (indexLista number '7) '(1) (indexLista number '9))  minValue maxValue)
+                )))
         ((equal? bitPos 2)
-         (cond ((equal? (+ (cadr number) 1) 2) (binarySum (append (append (append (list(car number)) '(0)) (list(caddr number))) (list(cadddr number))) (+ bitPos 1)))
+         (cond ((equal? (+ (car (indexLista number '7)) 1) 2)
+                (binarySum (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                   (indexLista number '5) (indexLista number '6) '(0) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
                (else
-                (dischardOverflow (append (append (list(car number)) '(1)) (cddr number)))
+                (dischardOverflow (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                   (indexLista number '5) (indexLista number '6) '(1) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
           )))
-        ((equal? (+ (car number) 1) 2) (append '(0) (cdr number)))
-              (else
-         (dischardOverflow (append '(1) (cdr number))))))
+        ((equal? bitPos 3)
+         (cond ((equal? (+ (car (indexLista number '6)) 1) 2)
+                (binarySum (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                   (indexLista number '5) '(0) (indexLista number '7) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
+               (else
+                (dischardOverflow (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                   (indexLista number '5) '(1) (indexLista number '7) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
+          )))
+        ((equal? bitPos 4)
+         (cond ((equal? (+ (car (indexLista number '5)) 1) 2)
+                (binarySum (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                    '(0) (indexLista number '6) (indexLista number '7) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
+               (else
+                (dischardOverflow (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                    '(1) (indexLista number '6)(indexLista number '7) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
+          )))
+        ((equal? bitPos 5)
+         (cond ((equal? (+ (car (indexLista number '4)) 1) 2)
+                (binarySum (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) '(0)
+                                    (indexLista number '5) (indexLista number '6) (indexLista number '7) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
+               (else
+                (dischardOverflow (append (indexLista number '0) (indexLista number '1) (indexLista number '2) (indexLista number '3) '(1)
+                                    (indexLista number '5) (indexLista number '6)(indexLista number '7) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
+          )))
+        ((equal? bitPos 6)
+         (cond ((equal? (+ (car (indexLista number '3)) 1) 2)
+                (binarySum (append (indexLista number '0) (indexLista number '1) (indexLista number '2) '(0) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6) (indexLista number '7) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
+               (else
+                (dischardOverflow (append (indexLista number '0) (indexLista number '1) (indexLista number '2) '(1) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6)(indexLista number '7) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
+          )))
+        ((equal? bitPos 7)
+         (cond ((equal? (+ (car (indexLista number '2)) 1) 2)
+                (binarySum (append (indexLista number '0) (indexLista number '1) '(0) (indexLista number '3) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6) (indexLista number '7) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
+               (else
+                (dischardOverflow (append (indexLista number '0) (indexLista number '1) '(1) (indexLista number '3) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6)(indexLista number '7) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
+          )))
+        ((equal? bitPos 8)
+         (cond ((equal? (+ (car (indexLista number '1)) 1) 2)
+                (binarySum (append (indexLista number '0) '(0) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6) (indexLista number '7) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
+               (else
+                (dischardOverflow (append (indexLista number '0) '(1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6)(indexLista number '7) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
+          )))
+        (else
+         (cond ((equal? (+ (car (indexLista number '0)) 1) 2)
+                (binarySum (append '(0) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6) (indexLista number '7) (indexLista number '8) (indexLista number '9))
+                           (+ bitPos 1) minValue maxValue))
+               (else
+                (dischardOverflow (append '(1) (indexLista number '1) (indexLista number '2) (indexLista number '3) (indexLista number '4)
+                                    (indexLista number '5) (indexLista number '6)(indexLista number '7) (indexLista number '8) (indexLista number '9)) minValue maxValue
+                 )
+          ))
+         )))
 
-(selection-aux '(4 4 2) 'CR)
+
+
+
+;;(selection-aux '(4 4 2) 'CR)
+;;(selection-aux '(5 4 1) 'SPA)
+;;(selection-aux '(3 4 3) 'ENG)
+
+;;(getPlayerPosX '(CR 5 forward 3 9 6 50 20 3))
+;;(getPlayerGen '(CR 5 forward 3 9 6 50 20 3))
+
+;;(convertBinary_aux '1000 10)
+;;(convertDecimal-aux '(0 1 1 1 1 1 1 1 1 1) 9)
+;;(dischardOverflow '(1 0 1 1))
+;;(dischardOverflowPosition '(0 1 1 1 1 1 1 1 1 1) '1000)
+
+;;(mutation-aux '(0 1 1 1) 0)
+;;(binarySum '(0 1 1 1 1 1 0 1 1 1) '8)
+
+;;(display "APTITUDE? ")
+;;(aptitude? '(CR 1 keeper 7 4 7 50 20 1))
+;;(aptitude? '(CR 5 defender 3 8 6 50 20 1))
+;;(aptitude? '(CR 2 mid 5 4 7 50 20 1))
+;;(aptitude? '(CR 3 forward 4 4 2 50 20 1))
+;;(mutation '(CR 5 forward 5 9 6 50 20 3))
+
+;;(binarySum '(1 0 0 1) '0)
+
+;;(aptitude? '(CR 5 defender 3 8 6 50 20 1))
+
+;(selection '(CR
+ ; (CR 1 keeper 9 1 8 50 20 1)
+  ;((CR 5 defender 3 7 2 50 20 1) (CR 4 defender 4 1 2 50 20 1) (CR 3 defender 7 5 9 50 20 1) (CR 2 defender 8 8 9 50 20 1))
+  ;((CR 5 mid 2 2 9 50 20 1) (CR 4 mid 7 1 5 50 20 1) (CR 3 mid 1 2 3 50 20 1) (CR 2 mid 9 5 3 50 20 1))
+  ;((CR 3 forward 1 1 2 50 20 1) (CR 2 forward 0 6 8 50 20 1))))
+
+;(updateTree '(CR
+;  (CR 1 keeper 9 1 8 50 20 1)
+;  ((CR 5 defender 3 7 2 50 20 1) (CR 4 defender 4 1 2 50 20 1) (CR 3 defender 7 5 9 50 20 1) (CR 2 defender 8 8 9 50 20 1))
+;  ((CR 5 mid 2 2 9 50 20 1) (CR 4 mid 7 1 5 50 20 1) (CR 3 mid 1 2 3 50 20 1) (CR 2 mid 9 5 3 50 20 1))
+;  ((CR 3 forward 1 1 2 50 20 1) (CR 2 forward 0 6 8 50 20 1)))
+ ;           (reproduce (selection '(CR
+;  (CR 1 keeper 9 1 8 50 20 1)
+ ; ((CR 5 defender 3 7 2 50 20 1) (CR 4 defender 4 1 2 50 20 1) (CR 3 defender 7 5 9 50 20 1) (CR 2 defender 8 8 9 50 20 1))
+;  ((CR 5 mid 2 2 9 50 20 1) (CR 4 mid 7 1 5 50 20 1) (CR 3 mid 1 2 3 50 20 1) (CR 2 mid 9 5 3 50 20 1))
+;  ((CR 3 forward 1 1 2 50 20 1) (CR 2 forward 0 6 8 50 20 1))))) '())
+
+
+;(reproduce '((CR 1 keeper 9 1 8 50 20 1) (CR 2 defender 8 8 9 50 20 1) (CR 3 defender 7 5 9 50 20 1) (CR 2 mid 9 5 3 50 20 1) (CR 2 forward 0 6 8 50 20 1)) '())
+
+;(updateTree '(CR
+;  (CR 1 keeper 9 1 8 50 20 1)
+;  ((CR 5 defender 3 7 2 50 20 1) (CR 4 defender 4 1 2 50 20 1) (CR 3 defender 7 5 9 50 20 1) (CR 2 defender 8 8 9 50 20 1))
+;  ((CR 6 mid 2 2 9 50 20 1) (CR 10 mid 7 1 5 50 20 1) (CR 3 mid 1 2 3 50 20 1) (CR 9 mid 9 5 3 50 20 1))
+ ; ((CR 3 forward 1 1 2 50 20 1) (CR 7 forward 0 6 8 50 20 1)))
+       ;     '((CR 3 defender 5 5 11 50 20 2) (CR 10 mid 11 5 1 50 20 2)) )
+
+;(replacePlayer '((CR 5 defender 3 7 2 50 20 1) (CR 4 defender 4 1 2 50 20 1) (CR 3 defender 7 5 9 50 20 1) (CR 2 defender 8 8 9 50 20 1))
+ ;              '(CR 3 defender 5 5 11 50 20 2))
+
+;(reproduction '(CR 5 defender 4 5 2 50 20 1) '(CR 4 defender 7 3 9 50 20 1))
+
+(geneticAlgorithm '(CR
+  (CR 1 keeper 9 1 8 50 20 1)
+  ((CR 5 defender 3 7 2 50 20 1) (CR 4 defender 4 1 2 50 20 1) (CR 3 defender 7 5 9 50 20 1) (CR 2 defender 8 8 9 50 20 1))
+  ((CR 5 mid 2 2 9 50 20 1) (CR 4 mid 7 1 5 50 20 1) (CR 3 mid 1 2 3 50 20 1) (CR 2 mid 9 5 3 50 20 1))
+  ((CR 3 forward 1 1 2 50 20 1) (CR 2 forward 0 6 8 50 20 1))))
+(convertDecimal-aux (mutatePos '(CR 1 keeper 3 9 6 50 120 1) 1) 9)
+
+;(mutation '((CR 3 defender 320 320 704 50 20 2) (CR 2 mid 704 320 64 50 20 2)))
+;(mutation '(CR 1 keeper 3 9 6 50 120 1))
+;(mutation '(CR 2 defender 3 9 6 250 100 1))
+;(mutation '(CR 3 midfielder 3 9 6 350 200 1))
+;(mutation '(CR 10 forward 3 9 6 640 150 1))
+;;(mutateSpecificGen '3 '0)
+;;(convertBinary_aux '1023 '10)
+;;(convertDecimal-aux '(0 0 0 0 0 0 1 0 1 0) '9)
+;;(convertDecimal (mutation-aux '(0 0 0 0 0 0 0 0 1 1) '2 '(0) '(10)))
